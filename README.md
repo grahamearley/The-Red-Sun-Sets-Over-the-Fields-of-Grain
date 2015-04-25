2fecdd3b1f8c1c483ea2292a2312490e4cd5dfd8
Graham Earley <graham.p.earley@gmail.com>
2015-04-24 21:39:17 -0500

    Initial commit
    Added Xcode initial files and a README.

---
+++ README.md
@@ -0,0 +1,4 @@
+# The Red Sun Rises Over the Fields of Grain
+A riveting iPhone game about the life of a farmer with a dark secret...
+
+*Made at CarlHacks 2015 (Carleton College)*
---
+++ The Red Sun Rises Over the Fields of GrainTests/The_Red_Sun_Rises_Over_the_Fields_of_GrainTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Red_Sun_Rises_Over_the_Fields_of_GrainTests.swift
+//  The Red Sun Rises Over the Fields of GrainTests
+//
+//  Created by Graham Earley on 4/24/15.
+//  Copyright (c) 2015 Silo Games. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class The_Red_Sun_Rises_Over_the_Fields_of_GrainTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
