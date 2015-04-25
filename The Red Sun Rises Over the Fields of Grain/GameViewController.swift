a3fd2442260c35b5981c9cd96edc026510484b83
Charlie Imhoff <imhoffc@carleton.edu>
2015-04-24 22:21:56 -0500

    Replaced defualt code.

--- The Red Sun Rises Over the Fields of Grain/GameViewController.swift
+++ The Red Sun Rises Over the Fields of Grain/GameViewController.swift
@@ -9,50 +9,31 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : String) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+		// Configure the view.
+		let skView = self.view as! SKView
+		let scene = GameScene(size:	skView.bounds.size)
+		skView.showsFPS = true
+		skView.showsNodeCount = true
+		skView.ignoresSiblingOrder = true
+		
+		/* Set the scale mode to scale to fit the window */
+		scene.scaleMode = .AspectFill
+		
+		skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
-        return true
+        return false
     }
 
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+            return Int(UIInterfaceOrientationMask.Portrait.rawValue)
         } else {
             return Int(UIInterfaceOrientationMask.All.rawValue)
         }
